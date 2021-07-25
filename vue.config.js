0d8283f86bfd5a8408ed96977c8307f06db345fd
Suveer Raina Desktop <14318177+suveer-r@users.noreply.github.com>
2021-07-26 03:32:15 +0530

    Updated the icons

--- vue.config.js
+++ vue.config.js
@@ -23,12 +23,12 @@ module.exports = {
     },
 
     iconPaths: {
-      favicon: null,
-      favicon32: "img/icons/maskable_icon_x32.png",
-      favicon16: "img/icons/maskable_icon_x16.png",
-      appleTouchIcon: "img/icons/maskable_icon_x192.png",
-      maskIcon: "img/icons/maskable_icon_x512.png",
-      msTileImage: "img/icons/maskable_icon_x128.png",
+      favicon: "img/icons/favicon.ico",
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon.png",
+      maskIcon: "img/icons/android-chrome-512x512.png",
+      msTileImage: "img/icons/android-chrome-192x192.png",
     },
 
     manifestOptions: {
@@ -49,37 +49,13 @@ module.exports = {
           purpose: "any maskable",
         },
         {
-          src: "img/icons/maskable_icon_x48.png",
-          sizes: "48x48",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-        {
-          src: "img/icons/maskable_icon_x72.png",
-          sizes: "72x72",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-        {
-          src: "img/icons/maskable_icon_x96.png",
-          sizes: "96x96",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-        {
-          src: "img/icons/maskable_icon_x128.png",
-          sizes: "128x128",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-        {
-          src: "img/icons/maskable_icon_x192.png",
+          src: "img/icons/android-chrome-192x192.png",
           sizes: "192x192",
           type: "image/png",
           purpose: "any maskable",
         },
         {
-          src: "img/icons/maskable_icon_x512.png",
+          src: "img/icons/android-chrome-512x512.png",
           sizes: "512x512",
           type: "image/png",
           purpose: "any maskable",
