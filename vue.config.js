a8fae086ac20cd148afec8d1c7a8db27d5dcde64
Suveer Raina Desktop <14318177+suveer-r@users.noreply.github.com>
2021-07-25 23:43:35 +0530

    manifestOptions

--- vue.config.js
+++ vue.config.js
@@ -30,6 +30,26 @@ module.exports = {
       maskIcon: "img/icons/maskable_icon_x512.png",
       msTileImage: "img/icons/maskable_icon_x128.png",
     },
+
+    manifestOptions: {
+      display: "standalone",
+      theme_color: "#00bfa9",
+      background_color: "#00a1b1",
+      icons: [
+        {
+          src: "img/icons/maskable_icon_x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "img/icons/maskable_icon_x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
   },
 
   lintOnSave: false,
