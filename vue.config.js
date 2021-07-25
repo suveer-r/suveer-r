a51cbec2bcb2650a6a88b67756bd5930ca706ffb
Suveer Raina Desktop <14318177+suveer-r@users.noreply.github.com>
2021-07-25 23:54:34 +0530

    updated vue config pwa

--- vue.config.js
+++ vue.config.js
@@ -37,6 +37,42 @@ module.exports = {
       background_color: "#00a1b1",
       icons: [
         {
+          src: "img/icons/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "img/icons/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "img/icons/maskable_icon_x48.png",
+          sizes: "48x48",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "img/icons/maskable_icon_x72.png",
+          sizes: "72x72",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "img/icons/maskable_icon_x96.png",
+          sizes: "96x96",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "img/icons/maskable_icon_x128.png",
+          sizes: "128x128",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
           src: "img/icons/maskable_icon_x192.png",
           sizes: "192x192",
           type: "image/png",
